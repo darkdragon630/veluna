9c7970bf3a59c0397b4451cbcbb8a766cb4d4aaa
darkdragon630 <71604929+darkdragon630@users.noreply.github.com>
2026-03-09 19:11:40 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
 define('BASE_URL', ((!empty($_SERVER['HTTPS'])&&$_SERVER['HTTPS']!=='off')?'https':'http').'://'.$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['SCRIPT_NAME']),'/').'/' );
+require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/config/config.php';
+// Hapus session dari DB dan destroy
+if (session_status() === PHP_SESSION_NONE) session_start();
 session_destroy();
 header('Location: ' . BASE_URL . 'index.php');
 exit;
